fb47530b69e16a3a23550957f9475f926d0a6314
Raphaël Thériault <raphael_theriault@outlook.com>
2019-12-02 16:21:10 -0500

    Multithread + no cheating

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
-use std::time::Instant;
-
-static CODE: &[usize] = include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
+use std::{env, fs, sync::mpsc, thread, time::Instant};
 
 #[inline(always)]
-fn run(n: usize, v: usize) -> usize {
-    let mut code = CODE.to_vec();
+fn parse_args() -> (usize, String, usize) {
+    let args: Vec<String> = env::args().collect();
+    let args_len = args.len();
+
+    let target_result = if args_len >= 2 {
+        args[1].parse().expect("Invalid target result")
+    } else {
+        panic!("Missing target result");
+    };
+    let input_file = if args_len >= 3 {
+        args[2].clone()
+    } else {
+        "input.txt".to_owned()
+    };
+    let thread_count = if args_len >= 4 {
+        args[3].parse().expect("Invalid thread count")
+    } else {
+        1
+    };
+
+    (target_result, input_file, thread_count)
+}
+
+#[inline(always)]
+fn parse_input_file(input_file: &str) -> Vec<usize> {
+    let contents = fs::read_to_string(input_file).expect("Invalid input file");
+    contents.split(',').filter_map(|n| n.parse().ok()).collect()
+}
+
+#[inline(always)]
+fn run(code: &[usize], n: usize, v: usize) -> usize {
+    let mut code = code.to_vec();
     code[1] = n;
     code[2] = v;
 
@@ -29,20 +57,37 @@ fn run(n: usize, v: usize) -> usize {
 fn main() {
     let start = Instant::now();
 
-    for n in 0..100 {
-        for v in 0..100 {
-            if run(n, v) == 19690720 {
-                println!("Result is {}", 100 * n + v);
+    let (target_result, input_file, thread_count) = parse_args();
+    let code = parse_input_file(&input_file);
 
-                let elapsed = start.elapsed();
-                println!(
-                    "Done in {}μs ({}ms)",
-                    elapsed.as_micros(),
-                    elapsed.as_millis()
-                );
+    let chunk_size = 100 / thread_count;
+    let range: Vec<usize> = (0..100).collect();
+    let chunks = range.chunks(chunk_size);
 
-                return;
+    let (tx, rx) = mpsc::channel();
+
+    for chunk in chunks {
+        let code = code.clone();
+        let chunk = chunk.to_vec();
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            for n in chunk {
+                for v in 0..100 {
+                    if run(&code, n, v) == target_result {
+                        tx.send(100 * n + v).unwrap();
+                    }
+                }
             }
-        }
+        });
     }
+
+    let result = rx.recv().unwrap();
+    let elapsed = start.elapsed();
+    println!("Result is {}", result);
+    println!(
+        "Done in {}μs ({}ms)",
+        elapsed.as_micros(),
+        elapsed.as_millis()
+    );
 }
